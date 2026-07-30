c730e3f1d8a2db0b57e36223e6a451a225b670e7
Allen Su <allensux@gmail.com>
2026-07-31 00:39:38 +0800

    feat: streamline deployment and health checks

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -51,8 +51,8 @@ resource "aws_lb_target_group" "web" {
     protocol            = "HTTP"
     port                = "traffic-port"
     matcher             = "200"
-    interval            = 30
-    timeout             = 5
+    interval            = 5
+    timeout             = 2
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
